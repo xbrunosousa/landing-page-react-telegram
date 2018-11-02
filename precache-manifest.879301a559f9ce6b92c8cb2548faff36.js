4954b4242875092ba056126046088dfb7fd6bf41
Bruno Vinícius <xbrunosousa@icloud.com>
2018-11-01 21:07:00 -0300

    Updates

---
+++ precache-manifest.879301a559f9ce6b92c8cb2548faff36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f443ca024e8a6fbfd33e94dbac747026",
+    "url": "/landing-page-react-telegram/static/media/telegram-mac-os-screenshot-min.f443ca02.png"
+  },
+  {
+    "revision": "eea7951940b94b43f714",
+    "url": "/landing-page-react-telegram/static/js/runtime~main.eea79519.js"
+  },
+  {
+    "revision": "ae87f1c031ea63d60d26",
+    "url": "/landing-page-react-telegram/static/js/main.ae87f1c0.chunk.js"
+  },
+  {
+    "revision": "4e11ce105bc5f55ba1d4",
+    "url": "/landing-page-react-telegram/static/js/1.4e11ce10.chunk.js"
+  },
+  {
+    "revision": "ae87f1c031ea63d60d26",
+    "url": "/landing-page-react-telegram/static/css/main.d3edebd2.chunk.css"
+  },
+  {
+    "revision": "a1ffe20eed39c6a933effc03e28657e6",
+    "url": "/landing-page-react-telegram/index.html"
+  }
+];
