167b536c996c5ce1cdf47a7f5fc5daa314c1448d
Bruno Sousa <xbrunosousa@icloud.com>
2019-11-18 03:53:49 -0300

    :sparkles: Use node-sass =D

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import './App.scss';
 import NavbarApp from './NavbarApp/navbar';
 import BannerHome from './BannerHome/bannerHome';
 import ContentApp from './ContentApp/contentApp';
